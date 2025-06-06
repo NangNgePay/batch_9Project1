146b425b5a87699d4e1910a38b8ff04dbd4bf0db
NangNgePay <ngepay685@gmail.com>
2025-06-07 01:05:54 +0630

    Update database.php

--- database.php
+++ database.php
@@ -1,13 +1,1 @@
-<?php
-// database.php
-$host = "localhost";
-$user = "root";
-$password = "";
-$dbname = "valentine_shop";
-
-$conn = new mysqli($host, $user, $password, $dbname);
-
-if ($conn->connect_error) {
-    die("Database connection failed: " . $conn->connect_error);
-}
-?>
+Remove unused Bootstrap files
