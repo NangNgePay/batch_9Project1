102f56ea61fb9515db2793a827e8e14c47624fc9
NangNgePay <ngepay685@gmail.com>
2025-06-07 01:06:56 +0630

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,13 +1,1 @@
-    </div> <!-- Close container div -->
-
-    <footer class="bg-dark text-white mt-5 p-4 text-center">
-        &copy; 2024 Valentine Gift Shop
-    </footer>
-
-    <!-- Bootstrap JS (with Popper.js included) -->
-    <!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script> -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
-    <!-- <script src="./js/bootstrap.min.js"></script> -->
-    <script src="./assets/js/main.js"></script>
-</body>
-</html>
+Remove unused Bootstrap files
